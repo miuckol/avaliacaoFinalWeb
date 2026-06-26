88dc89f14908f88bdbabf585f38a73edc55c4820
Caroline Lankenwicz <lanke.caroline45@gmail.com>
2026-06-26 00:19:19 -0300

    delete

--- avaliacaoFinalWeb/sair.php
+++
@@ -1,5 +0,0 @@
-<?php
-sess_start();
-session_destroy();
-header("Location: index.php");
-exit;
--- avaliacaoFinalWeb/salvar_pontuacao.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-echo "<pre>";
-print_r($_SESSION);
-
-echo "<br><br>";
-
-print_r($_POST);
